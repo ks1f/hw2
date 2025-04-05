23309505c9e0548d01e78f688afb94bb05ca4689
Ks1f <ks1fer123@gmail.com>
2025-04-05 18:50:09 +0200

    add my last home work

---
+++ index.js
@@ -0,0 +1,7 @@
+let username = prompt('What`s your name?');
+// let firstNumber =prompt('Please, enter any number')
+// let secondNumber=prompt('Please, enter any number again')
+// const result= (`${firstNumber} + ${secondNumber} = `)
+
+
+alert(`Hi, ${username}!, How are you?`);
